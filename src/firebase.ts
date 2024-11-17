75cbc02139b07a5e68651eae7843b702ecf202dc
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-11-17 22:32:45 +0100

    Void

--- src/firebase.ts
+++ src/firebase.ts
@@ -19,7 +19,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-// export const db = getFirestore(app)
 export const db = initializeFirestore(app, {
   localCache: persistentLocalCache({
     tabManager: persistentSingleTabManager({ forceOwnership: false }),
